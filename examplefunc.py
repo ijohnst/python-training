70686a7e5ca9897e8a50452b20def9633001c14b
ijohnst <mydevmachine@protonmail.com>
2021-12-15 18:24:45 -0500

    Add functions examples

---
+++ examplefunc.py
@@ -0,0 +1,25 @@
+def plusOne(number):
+    return number + 1
+
+
+newNumber = plusOne(5)
+print(newNumber)
+
+"""
+def hello(name):
+    print('Hello ' + name)
+
+
+hello('Alice')
+hello('Bob')
+
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there!')
+
+
+hello()
+hello()
+hello()
+"""
